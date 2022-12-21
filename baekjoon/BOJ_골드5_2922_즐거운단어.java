195c3f88d7f6350337da30e230ed3738cb7ddb74
turtlebooster <jhchoi6778@gmail.com>
2022-12-21 17:41:56 +0900

    BackTracking refactoring

--- baekjoon/BOJ_골드5_2922_즐거운단어.java
+++ baekjoon/BOJ_골드5_2922_즐거운단어.java
@@ -18,22 +18,7 @@ public class BOJ_골드5_2922_즐거운단어 {
         char c = word.charAt(0);
         boolean isL = c == 'L';
 
-        if (c != '_') {
-            if (isL) lCnt++;
-            boolean now = "AEIOU".contains(c + "");
-            recur(1, 1, now, 1);
-            if (isL) lCnt--;
-        } else {
-            // 모음 5 , 자음(L 제외) 20, L 1
-            // 모음
-            recur(1, 5, true, 1);
-            // 자음(L 제외)
-            recur(1, 20, false, 1);
-            // L
-            lCnt++;
-            recur(1, 1, false, 1);
-            lCnt--;
-        }
+        recur(0, 1, true, 0);
 
         System.out.println(ans);
         sc.close();
